34282021c215bc38b05d90b2c94367b6e13be85e
AntonSidarovich <87302265+AntonSidarovich@users.noreply.github.com>
2021-07-16 12:29:41 +0200

    Add files via upload

--- Restaurants/restaurants/admin.py
+++ Restaurants/restaurants/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 from .models import Restaurant
 
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'pub_date', 'update_date', 'is_published')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'pub_date', 'id', 'update_date')
 
-admin.site.register(Restaurant)
+admin.site.register(Restaurant, RestaurantAdmin)
--- Restaurants/restaurants/models.py
+++ Restaurants/restaurants/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=100)
-    describtion = models.TextField(blank=False)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
-    is_published = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, verbose_name = 'Nazwa restauracji', blank=False)
+    describtion = models.TextField(blank=False, verbose_name = 'Opis restauracji')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name = 'Data utworzenia')
+    update_date = models.DateTimeField(auto_now=True, verbose_name = 'Data ostatniej zmiany')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, verbose_name = 'Zdjęcie')
+    is_published = models.BooleanField(default=True, verbose_name = 'Opublikowana')
     def __str__(self):
         return self.name
 
- 
+    class Meta:
+        verbose_name = 'Restaurację'
+        verbose_name_plural = 'Restauracje'
+        ordering = ['-pub_date']
